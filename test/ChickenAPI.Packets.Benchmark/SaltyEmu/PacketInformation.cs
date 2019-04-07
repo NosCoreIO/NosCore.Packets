c42fb3ae6c8c0cc72917b53ccb903472c0d62978
Blowa <blowaxd693@gmail.com>
2019-04-07 12:57:18 +0800

    add basic benchmarks (#27) (0.4.0)
    * add basic benchmarks
    
    * fix compiling
    
    * move assembly to 0.4.0
    
    * fix benchmark runtime to 2.2

---
+++ test/ChickenAPI.Packets.Benchmark/SaltyEmu/PacketInformation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChickenAPI.Packets.Benchmark.SaltyEmu
+{
+    public class PacketInformation
+    {
+        public string Header { get; set; }
+        public Type Type { get; set; }
+
+        public PacketPropertyContainer[] PacketProps { get; set; }
+    }
+}
