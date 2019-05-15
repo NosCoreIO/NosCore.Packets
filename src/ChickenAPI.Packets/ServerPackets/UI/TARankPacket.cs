57bb0df1f0baf43661b7adf4d19423bc2203db11
Adán <22052411+Adanlink@users.noreply.github.com>
2019-05-15 19:00:04 +0200

    Add lots of NrunRunners, add WOpen, add TARank.
    Added lots of NrunRunnerTypes | Part1
    (There are still several of them missing, maybe I'll continue another day adding them. Please, if you see an NrunRunnerType named incorrectly fix it or communicate it to someone so it can be fixed.)
    
    Added WOpenPacket
    (Pretty amazing packet that the server sends to the client for various reasons, uses WindowTypes for the different types of windows that can be pooped out).
    
    Added WindowTypes
    (Extremely related with UpgradePacketTypes, but still more or less independent. If there is some of them missing please add it or if you can ascertain the UpgradeSpProtected do it).
    
    Added TARankPacket
    (The structure of this packet is pretty large, so I tried to do my best doing the packet. You can see that in the PacketIndex16 there is a string. It takes the datetime formated as string (yyMMddhhmm) of when the last month winners were fixed as winners)
    
    Added TARankSubPacket
    (Used by the TARankPacket to specify a character. If you are going to use this SubPacket to make a packet near perfection I would watchout with the PacketIndex3, as when it is null it will output "-" and that should be "-1" because Official NosTale wants.)
    
    "Added" some CancelPacketTypes.
    (Just btw)

---
+++ src/ChickenAPI.Packets/ServerPackets/UI/TARankPacket.cs
@@ -0,0 +1,59 @@
+﻿using ChickenAPI.Packets.Attributes;
+
+namespace ChickenAPI.Packets.ServerPackets.UI
+{
+    [PacketHeader("ta_rank")]
+    public class TARankPacket : PacketBase
+    {
+        [PacketIndex(0, SpecialSeparator = ".")]
+        public TARankSubPacket SwordsmanFirst { get; set; }
+
+        [PacketIndex(1, SpecialSeparator = ".")]
+        public TARankSubPacket SwordsmanSecond { get; set; }
+
+        [PacketIndex(2, SpecialSeparator = ".")]
+        public TARankSubPacket SwordsmanThird { get; set; }
+
+        [PacketIndex(3, SpecialSeparator = ".")]
+        public TARankSubPacket ArcherFirst { get; set; }
+
+        [PacketIndex(4, SpecialSeparator = ".")]
+        public TARankSubPacket ArcherSecond { get; set; }
+
+        [PacketIndex(5, SpecialSeparator = ".")]
+        public TARankSubPacket ArcherThird { get; set; }
+
+        [PacketIndex(6, SpecialSeparator = ".")]
+        public TARankSubPacket MageFirst { get; set; }
+
+        [PacketIndex(7, SpecialSeparator = ".")]
+        public TARankSubPacket MageSecond { get; set; }
+
+        [PacketIndex(8, SpecialSeparator = ".")]
+        public TARankSubPacket MageThird { get; set; }
+
+        [PacketIndex(9, SpecialSeparator = ".")]
+        public TARankSubPacket MartialArtistFirst { get; set; }
+
+        [PacketIndex(10, SpecialSeparator = ".")]
+        public TARankSubPacket MartialArtistSecond { get; set; }
+
+        [PacketIndex(11, SpecialSeparator = ".")]
+        public TARankSubPacket MartialArtistThird { get; set; }
+
+        [PacketIndex(12, SpecialSeparator = ".")]
+        public TARankSubPacket SwordsmanLastWinner { get; set; }
+
+        [PacketIndex(13, SpecialSeparator = ".")]
+        public TARankSubPacket ArcherLastWinner { get; set; }
+
+        [PacketIndex(14, SpecialSeparator = ".")]
+        public TARankSubPacket MageLastWinner { get; set; }
+
+        [PacketIndex(15, SpecialSeparator = ".")]
+        public TARankSubPacket MartialArtistLastWinner { get; set; }
+
+        [PacketIndex(16)]
+        public string Date { get; set; } // yyMMddhhmm
+    }
+}
