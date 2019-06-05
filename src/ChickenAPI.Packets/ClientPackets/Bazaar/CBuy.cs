3cfcd5382360a848ee56bd9a896f328cb3a2852c
0Lucifer0 <erwan.joly@gmail.com>
2019-06-05 23:42:45 +1200

    More bazaar packets (#100)
    * more bazaar packet
    
    * bump version to 0.9.1
    
    * fix types

---
+++ src/ChickenAPI.Packets/ClientPackets/Bazaar/CBuy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChickenAPI.Packets.Attributes;
+
+namespace ChickenAPI.Packets.ClientPackets.Bazaar
+{
+    [PacketHeader("c_buy")]
+    public class CBuyPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public long BazaarId { get; set; }
+
+        [PacketIndex(1)]
+        public short VNum { get; set; }
+
+        [PacketIndex(2)]
+        public short Amount { get; set; }
+
+        [PacketIndex(3)]
+        public long Price { get; set; }
+    }
+}
