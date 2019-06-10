0a782880e416e7ab5bb44fe65e0cf471d0ff6e82
0Lucifer0 <erwan.joly@gmail.com>
2019-06-10 21:19:11 +1200

    Fix rcs list element packet (#110)
    * fix rcslist
    
    * bump to 0.9.3

---
+++ src/ChickenAPI.Packets/Enumerations/BazaarStatusType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChickenAPI.Packets.Enumerations
+{
+    public enum BazaarStatusType : byte
+    {
+        OnSale = 1,
+        Solded = 2,
+        DelayExpired = 3
+    }
+}
