fb0b80ca994e6788f424a16e62fffd539f78e072
0Lucifer0 <erwan.joly@gmail.com>
2019-08-04 10:35:32 +1200

    fix qset client

--- src/ChickenAPI.Packets/ServerPackets/Quicklist/QsetClientPacket.cs
+++ src/ChickenAPI.Packets/ServerPackets/Quicklist/QsetClientPacket.cs
@@ -7,15 +7,12 @@ namespace ChickenAPI.Packets.ServerPackets.Quicklist
     public class QsetClientPacket : PacketBase
     {
         [PacketIndex(0)]
-        public QSetType Type { get; set; }
-
-        [PacketIndex(1)]
         public short OriginQuickList { get; set; }
 
-        [PacketIndex(2)]
+        [PacketIndex(1)]
         public short OriginQuickListSlot { get; set; }
 
-        [PacketIndex(3)]
+        [PacketIndex(2)]
         public QsetClientSubPacket Data { get; set; }
     }
 }
