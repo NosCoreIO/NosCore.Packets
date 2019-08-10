7dc5de61025bd14ca29e56ebef59729537107838
0Lucifer0 <erwan.joly@gmail.com>
2019-08-10 21:56:08 +1200

    add some packets

---
+++ src/ChickenAPI.Packets/ServerPackets/Miniland/MlintroPacket.cs
@@ -0,0 +1,11 @@
+﻿using ChickenAPI.Packets.Attributes;
+
+namespace ChickenAPI.Packets.ServerPackets.Miniland
+{
+    [PacketHeader("mlintro")]
+    public class MlintroPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public string Intro { get; set; }
+    }
+}
