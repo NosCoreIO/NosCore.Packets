7ed9206982fa84acad9cb22470c987710e9d4c65
0Lucifer0 <erwan.joly@gmail.com>
2019-08-10 21:56:08 +1200

    add some packets

---
+++ src/ChickenAPI.Packets/ServerPackets/Miniland/MloInfoPacket.cs
@@ -0,0 +1,29 @@
+﻿using ChickenAPI.Packets.Attributes;
+
+namespace ChickenAPI.Packets.ServerPackets.Miniland
+{
+    [PacketHeader("mlo_info")]
+    public class MloInfoPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public bool IsOwner { get; set; }
+
+        [PacketIndex(1)]
+        public short ObjectVNum { get; set; }
+
+        [PacketIndex(2)]
+        public byte Slot { get; set; }
+
+        [PacketIndex(3)]
+        public long MinilandPoints { get; set; }
+
+        [PacketIndex(4)]
+        public bool LawDurability { get; set; }
+
+        [PacketIndex(5)]
+        public bool IsFull { get; set; }
+
+        [PacketIndex(6, Length = 5, SpecialSeparator = " ")]
+        public MloInfoPacketSubPacket[] MinigamePoints { get; set; }
+    }
+}
---
+++ src/ChickenAPI.Packets/ServerPackets/Miniland/MloInfoPacketSubPacket.cs
@@ -0,0 +1,13 @@
+﻿using ChickenAPI.Packets.Attributes;
+
+namespace ChickenAPI.Packets.ServerPackets.Miniland
+{
+    public class MloInfoPacketSubPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public long MinimumPoints { get; set; }
+
+        [PacketIndex(1)]
+        public long MaximumPoints { get; set; }
+    }
+}
