29a8c98bbc2eebbbe27a89d32380379a25699b56
0Lucifer0 <erwan.joly@gmail.com>
2019-08-21 13:54:17 +1200

    some optimisation

--- src/ChickenAPI.Packets/ServerPackets/Miniland/MlobjlstSubPacket.cs
+++ src/ChickenAPI.Packets/ServerPackets/Miniland/MlobjlstSubPacket.cs
@@ -1,4 +1,5 @@
 ﻿using ChickenAPI.Packets.Attributes;
+using ChickenAPI.Packets.ClientPackets.Miniland;
 
 namespace ChickenAPI.Packets.ServerPackets.Miniland
 {
@@ -7,31 +8,7 @@ namespace ChickenAPI.Packets.ServerPackets.Miniland
         [PacketIndex(0)]
         public short Slot { get; set; }
 
-        [PacketIndex(1)]
-        public bool InUse { get; set; }
-
-        [PacketIndex(2)]
-        public short MapX { get; set; }
-
-        [PacketIndex(3)]
-        public short MapY { get; set; }
-
-        [PacketIndex(4)]
-        public byte Width { get; set; }
-
-        [PacketIndex(5)]
-        public byte Height { get; set; }
-
-        [PacketIndex(6)]
-        public int DurabilityPoint { get; set; }
-
-        [PacketIndex(7)]
-        public byte Unknown { get; set; }
-
-        [PacketIndex(8)]
-        public bool Unknown2 { get; set; }
-
-        [PacketIndex(9)]
-        public bool Unknown3 { get; set; }
+        [PacketIndex(1, RemoveHeader = true, SpecialSeparator = ".")]
+        public MlobjPacket MlObjSubPacket { get; set; }
     }
 }
