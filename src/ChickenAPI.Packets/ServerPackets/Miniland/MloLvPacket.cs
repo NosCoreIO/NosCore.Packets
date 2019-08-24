6a93bed85bfbfd1f38ccbc0047da92de9bcc0698
0Lucifer0 <erwan.joly@gmail.com>
2019-08-24 14:14:10 +1200

    add minigame packets

---
+++ src/ChickenAPI.Packets/ServerPackets/Miniland/MloLvPacket.cs
@@ -0,0 +1,11 @@
+﻿using ChickenAPI.Packets.Attributes;
+
+namespace ChickenAPI.Packets.ServerPackets.Miniland
+{
+    [PacketHeader("mlo_lv")]
+    public class MloLvPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public short Level { get; set; }
+    }
+}
---
+++ src/ChickenAPI.Packets/ServerPackets/Miniland/MloPmgSubPacket.cs
@@ -0,0 +1,13 @@
+﻿using ChickenAPI.Packets.Attributes;
+
+namespace ChickenAPI.Packets.ServerPackets.Miniland
+{
+    public class MloPmgSubPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public short BoxVNum { get; set; }
+
+        [PacketIndex(1)]
+        public long BoxAmount { get; set; }
+    }
+}
