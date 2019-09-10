96a7eb7418a3ebea1e4bd99c8724ff4787b2d1dd
0Lucifer0 <erwan.joly@gmail.com>
2019-09-10 21:06:22 +1200

    Fix packet pst

---
+++ src/ChickenAPI.Packets/ClientPackets/Parcel/PstClientPacket.cs
@@ -0,0 +1,31 @@
+﻿using ChickenAPI.Packets.Attributes;
+
+namespace ChickenAPI.Packets.ClientPackets.Parcel
+{
+    public class PstClientPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public int ActionType { get; set; }
+
+        [PacketIndex(1)]
+        public int Type { get; set; }
+
+        [PacketIndex(2)]
+        public long Id { get; set; }
+
+        [PacketIndex(3)]
+        public int? Unknow1 { get; set; }
+
+        [PacketIndex(4)]
+        public int Unknow2 { get; set; }
+
+        [PacketIndex(5)]
+        public string ReceiverName { get; set; }
+
+        [PacketIndex(6, IsOptional = true)]
+        public string Title { get; set; }
+
+        [PacketIndex(7, IsOptional = true)]
+        public string Text { get; set; }
+    }
+}
