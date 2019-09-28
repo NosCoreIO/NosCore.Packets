84724c952ef4f832fcece7d241fe7fa3b2323f66
0Lucifer0 <erwan.joly@gmail.com>
2019-09-28 16:58:02 +1200

    add client packets for warehouse

---
+++ src/ChickenAPI.Packets/ServerPackets/Warehouse/FStashAllPacket.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using ChickenAPI.Packets.Attributes;
+using ChickenAPI.Packets.Enumerations;
+using ChickenAPI.Packets.ServerPackets.Inventory;
+
+namespace ChickenAPI.Packets.ServerPackets.Warehouse
+{
+    [PacketHeader("f_stash_all")]
+    public class FStashAllPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public byte WarehouseSize { get; set; }
+
+        [PacketIndex(1)]
+        public List<IvnSubPacket> IvnSubPackets { get; set; }
+    }
+}
