6f776c7035174a3139160185a7a6c39ebb034a7c
0Lucifer0 <erwan.joly@gmail.com>
2019-10-25 12:57:46 +1300

    fix guri

--- src/ChickenAPI.Packets/Enumerations/GuriPacketType.cs
+++ src/ChickenAPI.Packets/Enumerations/GuriPacketType.cs
@@ -4,8 +4,17 @@
     {
         Unknow = 2,
         Unknow2 = 6,
-        Unknow3 = 10,
+        Effect = 10,
         Unknow4 = 15,
-        AfterSumming = 19
+        AfterSumming = 19,
+        ShellIdentification = 204,
+        Perfume = 205,
+        IceBreaker = 501,
+        IceBreakerFrozen = 502,
+        EventInWaiting = 506,
+        Title = 306,
+        PartnerBackpack = 202,
+        PetBasket = 201,
+        SpPointInitializer = 203
     }
 }
