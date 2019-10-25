b07614699c0e27c3c21c186f6ad8f61bfc716086
0Lucifer0 <erwan.joly@gmail.com>
2019-10-25 13:00:31 +1300

    add some guri types

--- src/ChickenAPI.Packets/Enumerations/GuriPacketType.cs
+++ src/ChickenAPI.Packets/Enumerations/GuriPacketType.cs
@@ -7,14 +7,14 @@
         Effect = 10,
         Unknow4 = 15,
         AfterSumming = 19,
+        PetBasket = 201,
+        PartnerBackpack = 202,
+        SpPointInitializer = 203,
         ShellIdentification = 204,
         Perfume = 205,
+        Title = 306,
         IceBreaker = 501,
         IceBreakerFrozen = 502,
         EventInWaiting = 506,
-        Title = 306,
-        PartnerBackpack = 202,
-        PetBasket = 201,
-        SpPointInitializer = 203
     }
 }
