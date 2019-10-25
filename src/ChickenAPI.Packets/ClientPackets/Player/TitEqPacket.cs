2782a3d14d09781fe006fa4c737455d6c33e2370
0Lucifer0 <erwan.joly@gmail.com>
2019-10-25 13:07:06 +1300

    add titeq packet

---
+++ src/ChickenAPI.Packets/ClientPackets/Player/TitEqPacket.cs
@@ -0,0 +1,14 @@
+﻿using ChickenAPI.Packets.Attributes;
+
+namespace ChickenAPI.Packets.ServerPackets.Player
+{
+    [PacketHeader("tit_eq")]
+    public class TitEqPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public byte Mode { get; set; }
+
+        [PacketIndex(1)]
+        public short TitleId { get; set; }
+    }
+}
