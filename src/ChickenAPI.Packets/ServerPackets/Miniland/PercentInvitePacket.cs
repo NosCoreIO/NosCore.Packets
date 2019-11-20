18a40527f61bf05c31e75c3d22b10337b87d1e94
0Lucifer0 <erwan.joly@gmail.com>
2019-11-20 19:17:05 +1300

    add aliases

---
+++ src/ChickenAPI.Packets/ServerPackets/Miniland/PercentInvitePacket.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using ChickenAPI.Packets.Attributes;
+
+namespace ChickenAPI.Packets.ServerPackets.Miniland
+{
+    [PacketHeader("%Invite")]
+    [PacketHeaderAlias("$Inviter", Flag = "FR")]
+    public class PercentInvitePacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public string Username { get; set; }
+    }
+}
