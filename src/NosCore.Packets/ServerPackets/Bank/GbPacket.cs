04f17862e8db81aee8120ddc2a4abf13ec8b8e9d
Elendan <arthur.despeaux@epitech.eu>
2020-05-30 02:15:25 +0530

    Add "gb" bank packet (#47)
    * Add gb bank packet
    
    * Add missing ranges and update "gb" Type
    
    * update bank opening type
    
    * gold from long -> int

---
+++ src/NosCore.Packets/ServerPackets/Bank/GbPacket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using NosCore.Packets.Attributes;
+using NosCore.Packets.Enumerations;
+
+namespace NosCore.Packets.ServerPackets.Bank
+{
+    [PacketHeader("gb")]
+    public class GbPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public BankOpeningType Type { get; set; }
+
+        [PacketIndex(1)]
+        [Range(0, long.MaxValue)]
+        public long GoldBank { get; set; }
+
+        [PacketIndex(2)]
+        [Range(0, int.MaxValue)]
+        public int Gold { get; set; }
+
+        [PacketIndex(3)]
+        public BankRankType BankRank { get; set; }
+
+        [PacketIndex(4)]
+        [Range(0, int.MaxValue)]
+        public int BankTax { get; set; }
+    }
+}
