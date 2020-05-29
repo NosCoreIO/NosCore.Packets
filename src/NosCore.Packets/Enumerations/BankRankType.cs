751b1dba19a016dc6d0d077a1657e9b2689cbf3e
Elendan <arthur.despeaux@epitech.eu>
2020-05-29 22:22:22 +0200

    Add gb bank packet

---
+++ src/NosCore.Packets/Enumerations/BankRankType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NosCore.Packets.Enumerations
+{
+    public enum BankRankType : byte
+    {
+        Vip = 0,
+        Diamond1 = 1,
+        Diamond2 = 2,
+        Gold1 = 3,
+        Gold2 = 4,
+        Gold3 = 5,
+        Silver1 = 6,
+        Silver2 = 7,
+        Silver3 = 8,
+        Bronze1 = 9,
+        Bronze2 = 10,
+        Bronze3 = 11
+    }
+}
---
+++ src/NosCore.Packets/ServerPackets/Bank/GbPacket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NosCore.Packets.Attributes;
+using NosCore.Packets.Enumerations;
+
+namespace NosCore.Packets.ServerPackets.Bank
+{
+    [PacketHeader("gb")]
+    public class GbPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public byte Type { get; set; }
+
+        [PacketIndex(1)]
+        public long GoldBank { get; set; }
+
+        [PacketIndex(2)]
+        public long Gold { get; set; }
+
+        [PacketIndex(3)]
+        public BankRankType BankRank { get; set; }
+
+        [PacketIndex(4)]
+        public int BankTax { get; set; }
+    }
+}
