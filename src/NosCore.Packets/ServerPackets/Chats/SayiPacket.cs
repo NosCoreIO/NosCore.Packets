004903a7657fe2094fe101ea82f06cb1e6429a0b
erwan-joly <erwan.joly@pushpay.com>
2020-05-30 14:15:16 +1200

    add some i18n packets

---
+++ src/NosCore.Packets/ServerPackets/Chats/SayiPacket.cs
@@ -0,0 +1,52 @@
+﻿//  __  _  __    __   ___ __  ___ ___
+// |  \| |/__\ /' _/ / _//__\| _ \ __|
+// | | ' | \/ |`._`.| \_| \/ | v / _|
+// |_|\__|\__/ |___/ \__/\__/|_|_\___|
+// 
+// Copyright (C) 2019 - NosCore
+// 
+// NosCore is a free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using NosCore.Packets.Attributes;
+using NosCore.Packets.Enumerations;
+
+namespace NosCore.Packets.ServerPackets.Chats
+{
+    [PacketHeader("sayi")]
+    public class SayiPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public VisualType VisualType { get; set; }
+
+        [PacketIndex(1)]
+        public long VisualId { get; set; }
+
+        [PacketIndex(2)]
+        public SayColorType Type { get; set; }
+
+        [PacketIndex(3)]
+        public Game18NConstString Message { get; set; }
+
+        [PacketIndex(4)]
+        public short FirstArgument { get; set; }
+
+        [PacketIndex(5)]
+        public short SecondArgument { get; set; }
+
+        [PacketIndex(6)]
+        public short ThirdArgument { get; set; }
+
+        [PacketIndex(7)]
+        public short FifthArgument { get; set; }
+    }
+}
---
+++ src/NosCore.Packets/ServerPackets/Shop/SMemoi2Packet.cs
@@ -0,0 +1,27 @@
+﻿using NosCore.Packets.Attributes;
+using NosCore.Packets.Enumerations;
+
+namespace NosCore.Packets.ServerPackets.Shop
+{
+    [PacketHeader("s_memoi2")]
+    public class SMemoi2Packet : PacketBase
+    {
+        [PacketIndex(0)]
+        public SMemoType Type { get; set; }
+
+        [PacketIndex(1)]
+        public Game18NConstString Message { get; set; }
+
+        [PacketIndex(2)]
+        public short FirstArgument { get; set; }
+
+        [PacketIndex(3)]
+        public string? SecondArgument { get; set; }
+
+        [PacketIndex(4)]
+        public string ThirdArgument { get; set; } = "";
+
+        [PacketIndex(5)]
+        public short FourthArgument { get; set; }
+    }
+}
