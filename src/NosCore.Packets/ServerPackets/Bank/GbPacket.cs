f99448924cb85c68d5eb5d0bb3698bfea264d63b
Elendan <arthur.despeaux@epitech.eu>
2020-05-30 22:05:18 +0200

    Remove BankRankType enum and bump NosCore.Shared from 1.5.0 to 1.5.1

--- src/NosCore.Packets/ServerPackets/Bank/GbPacket.cs
+++ src/NosCore.Packets/ServerPackets/Bank/GbPacket.cs
@@ -20,6 +20,7 @@
 using System.ComponentModel.DataAnnotations;
 using NosCore.Packets.Attributes;
 using NosCore.Packets.Enumerations;
+using NosCore.Shared.Enumerations;
 
 namespace NosCore.Packets.ServerPackets.Bank
 {
