60375370fe9a767885721c9a85bae706f1203b21
erwan-joly <erwan.joly@pushpay.com>
2020-10-18 13:38:46 +1300

    add new packets for channel change

---
+++ src/NosCore.Packets/ServerPackets/Login/MzPacket.cs
@@ -0,0 +1,23 @@
+﻿//  __  _  __    __   ___ __  ___ ___
+// |  \| |/__\ /' _/ / _//__\| _ \ __|
+// | | ' | \/ |`._`.| \_| \/ | v / _|
+// |_|\__|\__/ |___/ \__/\__/|_|_\___|
+// -----------------------------------
+
+using NosCore.Packets.Attributes;
+
+namespace NosCore.Packets.ServerPackets.Login
+{
+    [PacketHeader("mz")]
+    public class MzPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public string Ip { get; set; } = null!;
+
+        [PacketIndex(1)]
+        public short Port { get; set; }
+
+        [PacketIndex(2)]
+        public byte CharacterSlot { get; set; }
+    }
+}
