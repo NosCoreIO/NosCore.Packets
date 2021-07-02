1f32aa4342c1964f52b94ad333c6ba1b4c579490
Fizo55 <hugomacees56000@gmail.com>
2021-07-02 16:05:34 +0200

    Add isort packet

---
+++ src/NosCore.Packets/ClientPackets/Inventory/ISortPacket.cs
@@ -0,0 +1,12 @@
+﻿using NosCore.Packets.Attributes;
+using NosCore.Packets.Enumerations;
+
+namespace NosCore.Packets.ClientPackets.Inventory
+{
+    [PacketHeader("isort", Scope.InGame)]
+    public class ISortPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public PocketType PocketType { get; set; }
+    }
+}
