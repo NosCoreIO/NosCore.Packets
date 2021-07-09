04a72e90680d68c5b5566130e4945c46109b06e3
Xwilarg <xwilarg@yahoo.fr>
2021-07-10 00:23:04 +0200

    Add Wreq

---
+++ src/NosCore.Packets/ClientPackets/Instance/WreqPacket.cs
@@ -0,0 +1,15 @@
+﻿using NosCore.Packets.Attributes;
+using NosCore.Packets.Enumerations;
+
+namespace NosCore.Packets.ClientPackets.Instance
+{
+    [PacketHeader("wreq", Scope.InGame)]
+    public class WreqPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public byte Value { get; set; }
+
+        [PacketIndex(1)]
+        public long? Param { get; set; }
+    }
+}
