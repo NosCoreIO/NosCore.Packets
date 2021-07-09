755ab58c175dd4a4f993e3a2d50a48aa99a65b77
Xwilarg <xwilarg@yahoo.fr>
2021-07-10 00:08:16 +0200

    Add missing packets

---
+++ src/NosCore.Packets/ClientPackets/Instance/TawPacket.cs
@@ -0,0 +1,12 @@
+﻿using NosCore.Packets.Attributes;
+using NosCore.Packets.Enumerations;
+
+namespace NosCore.Packets.ClientPackets.Instance
+{
+    [PacketHeader("taw", Scope.InGame)]
+    public class TawPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public string Username { get; set; } = null!;
+    }
+}
