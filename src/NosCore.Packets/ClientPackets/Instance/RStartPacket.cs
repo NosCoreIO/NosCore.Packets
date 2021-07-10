66453c609e8b3e018f81397147a02a093be37b49
Fizo55 <hugomacees56000@gmail.com>
2021-07-10 14:16:54 +0200

    Add rstart packet type

--- src/NosCore.Packets/ClientPackets/Instance/RStartPacket.cs
+++ src/NosCore.Packets/ClientPackets/Instance/RStartPacket.cs
@@ -1,4 +1,10 @@
-﻿using NosCore.Packets.Attributes;
+﻿//  __  _  __    __   ___ __  ___ ___
+// |  \| |/__\ /' _/ / _//__\| _ \ __|
+// | | ' | \/ |`._`.| \_| \/ | v / _|
+// |_|\__|\__/ |___/ \__/\__/|_|_\___|
+// -----------------------------------
+
+using NosCore.Packets.Attributes;
 using NosCore.Packets.Enumerations;
 
 namespace NosCore.Packets.ClientPackets.Instance
@@ -7,6 +13,6 @@ namespace NosCore.Packets.ClientPackets.Instance
     public class RStartPacket : PacketBase
     {
         [PacketIndex(0)]
-        public byte? Type { get; set; }
+        public RStartPacketType Type { get; set; }
     }
 }
--- src/NosCore.Packets/ClientPackets/Instance/SreqPacket.cs
+++ src/NosCore.Packets/ClientPackets/Instance/SreqPacket.cs
@@ -1,4 +1,10 @@
-﻿using NosCore.Packets.Attributes;
+﻿//  __  _  __    __   ___ __  ___ ___
+// |  \| |/__\ /' _/ / _//__\| _ \ __|
+// | | ' | \/ |`._`.| \_| \/ | v / _|
+// |_|\__|\__/ |___/ \__/\__/|_|_\___|
+// -----------------------------------
+
+using NosCore.Packets.Attributes;
 using NosCore.Packets.Enumerations;
 
 namespace NosCore.Packets.ClientPackets.Instance
