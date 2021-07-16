b02a674789adb03a386597d96c855588400a311d
Fizo55 <hugomacees56000@gmail.com>
2021-07-16 15:53:23 +0200

    Add missing eqinfo args

--- src/NosCore.Packets/ServerPackets/Player/EquipmentInfoPacket.cs
+++ src/NosCore.Packets/ServerPackets/Player/EquipmentInfoPacket.cs
@@ -15,11 +15,14 @@ namespace NosCore.Packets.ServerPackets.Player
     {
         [PacketIndex(0)]
         public byte Type { get; set; } // todo more information
-
+        
         [PacketIndex(1)]
         public short Slot { get; set; }
 
         [PacketIndex(2)]
+        public short MateSlot { get; set; }
+
+        [PacketIndex(3)]
         public long? ShopOwnerId { get; set; }
     }
 }
