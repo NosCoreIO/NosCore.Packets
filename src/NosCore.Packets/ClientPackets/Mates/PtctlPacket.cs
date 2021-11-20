393119226103e1e68b3cf485ce70a03b5842dab8
erwan-joly <erwan.joly@pushpay.com>
2021-11-20 18:37:50 +1300

    add more packets

---
+++ src/NosCore.Packets/ClientPackets/Mates/PtctlPacket.cs
@@ -0,0 +1,25 @@
+﻿//  __  _  __    __   ___ __  ___ ___
+// |  \| |/__\ /' _/ / _//__\| _ \ __|
+// | | ' | \/ |`._`.| \_| \/ | v / _|
+// |_|\__|\__/ |___/ \__/\__/|_|_\___|
+// -----------------------------------
+
+using NosCore.Packets.Attributes;
+using NosCore.Packets.Enumerations;
+using NosCore.Packets.Interfaces;
+
+namespace NosCore.Packets.ClientPackets.Mates
+{
+    [PacketHeader("ptctl", Scope.InGame)]
+    public class PtctlPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public short MapType { get; set; }
+
+        [PacketIndex(1)]
+        public short Amount { get; set; }
+
+        [PacketIndex(2)]
+        public string? PacketEnd { get; set; }
+    }
+}
---
+++ src/NosCore.Packets/ClientPackets/Mates/SayPPacket.cs
@@ -0,0 +1,22 @@
+﻿//  __  _  __    __   ___ __  ___ ___
+// |  \| |/__\ /' _/ / _//__\| _ \ __|
+// | | ' | \/ |`._`.| \_| \/ | v / _|
+// |_|\__|\__/ |___/ \__/\__/|_|_\___|
+// -----------------------------------
+
+using NosCore.Packets.Attributes;
+using NosCore.Packets.Enumerations;
+using NosCore.Packets.Interfaces;
+
+namespace NosCore.Packets.ClientPackets.Mates
+{
+    [PacketHeader("say_p", Scope.InGame)]
+    public class SayPPacket : PacketBase
+    {
+        [PacketIndex(0)]
+        public long PetId { get; set; }
+
+        [PacketIndex(1)]
+        public string? Message { get; set; }
+    }
+}
