c1212878ecc79c19e4c78e2ae196fa6b97fbb55a
erwan-joly <erwan.joly@pushpay.com>
2021-11-27 12:03:53 +1300

    fix #220

---
+++ src/NosCore.Packets/ServerPackets/Event/FbtPacket.cs
@@ -0,0 +1,18 @@
+﻿using NosCore.Packets.Attributes;
+using NosCore.Packets.Enumerations;
+
+namespace NosCore.Packets.ServerPackets.Event
+{
+    [PacketHeader("fbt", Scope.InGame)]
+    public class FbtPacket
+    {
+        [PacketIndex(0)]
+        public byte Type { get; set; }
+        [PacketIndex(1)]
+        public long Argument { get; set; }
+        [PacketIndex(2)]
+        public byte? Parameter { get; set; }
+        [PacketIndex(3)]
+        public short? SecondParameter { get; set; }
+    }
+}
