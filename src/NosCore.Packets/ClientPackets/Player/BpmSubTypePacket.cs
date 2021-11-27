ecac6208a81fe1bfaa1721e2fc285560096756df
Fizo55 <hugomacees56000@gmail.com>
2021-11-27 23:03:38 +0100

    Fix bpm subpacket

--- src/NosCore.Packets/ClientPackets/Player/BpmSubTypePacket.cs
+++ src/NosCore.Packets/ClientPackets/Player/BpmSubTypePacket.cs
@@ -11,21 +11,24 @@ namespace NosCore.Packets.ClientPackets.Player
     public  class BpmSubTypePacket : PacketBase
     {
         [PacketIndex(0)]
-        public short MissionSubType { get; set; } // todo : to enum
+        public long QuestId { get; set; }
 
         [PacketIndex(1)]
-        public short MissionType { get; set; } // todo : to enum
+        public short MissionSubType { get; set; } // todo : to enum
 
         [PacketIndex(2)]
-        public long Advencement { get; set; }
+        public short MissionType { get; set; } // todo : to enum
 
         [PacketIndex(3)]
-        public long MaxObjectiveValue { get; set; }
+        public long Advencement { get; set; }
 
         [PacketIndex(4)]
-        public byte Reward { get; set; }
+        public long MaxObjectiveValue { get; set; }
 
         [PacketIndex(5)]
+        public byte Reward { get; set; }
+
+        [PacketIndex(6)]
         public long MissionMinutesRemaining { get; set; }
     }
 }
