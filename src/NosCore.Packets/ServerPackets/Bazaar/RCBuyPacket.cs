06a84cc0a011f0efe0589a3392d45bd8ae848323
Worldie1 <AxeTrix@hotmail.com>
2022-01-06 09:35:26 +0100

    Update RCBuyPacket.cs

--- src/NosCore.Packets/ServerPackets/Bazaar/RCBuyPacket.cs
+++ src/NosCore.Packets/ServerPackets/Bazaar/RCBuyPacket.cs
@@ -16,19 +16,26 @@ namespace NosCore.Packets.ServerPackets.Bazaar
     {
         [PacketIndex(0)]
         public VisualType Type { get; set; }
+
         [PacketIndex(1)]
         public short VNum { get; set; }
+
         [PacketIndex(2)]
-        public long Owner { get; set; }
+        public string Owner { get; set; } = null!;
+
         [PacketIndex(3)]
         public short Amount { get; set; }
+
         [PacketIndex(4)]
         public long Price { get; set; }
+
         [PacketIndex(5)]
-        public byte Unknown1 { get; set; }
+        public short Slot { get; set; }
+
         [PacketIndex(6)]
-        public int Unknown2 { get; set; }
+        public byte Upgrade { get; set; }
+
         [PacketIndex(7)]
-        public byte Unknown3 { get; set; }
+        public byte Rarity { get; set; }
     }
 }
