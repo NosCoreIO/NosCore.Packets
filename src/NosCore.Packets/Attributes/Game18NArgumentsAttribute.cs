b9cf91eac0a7553be787803365b789d09b30e68f
erwan-joly <erwan.joly@pushpay.com>
2022-01-08 16:03:15 +1300

    add game i18n arguments

---
+++ src/NosCore.Packets/Attributes/Game18NArgumentsAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NosCore.Packets.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class Game18NArgumentsAttribute : Attribute
+    {
+        public Game18NArgumentsAttribute(params Type[] argumentTypes)
+        {
+            ArgumentTypes = argumentTypes;
+        }
+
+        public Type[] ArgumentTypes { get; set; }
+    }
+}
