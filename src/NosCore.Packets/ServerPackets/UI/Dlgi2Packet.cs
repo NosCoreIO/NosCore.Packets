f41f5c5e3a8ed83e0e8fdf16d4f70fcc9534a762
Worldie1 <AxeTrix@hotmail.com>
2022-01-09 00:57:32 +0100

    Add Dlgi2Packet

---
+++ src/NosCore.Packets/ServerPackets/UI/Dlgi2Packet.cs
@@ -0,0 +1,33 @@
+﻿//  __  _  __    __   ___ __  ___ ___
+// |  \| |/__\ /' _/ / _//__\| _ \ __|
+// | | ' | \/ |`._`.| \_| \/ | v / _|
+// |_|\__|\__/ |___/ \__/\__/|_|_\___|
+// -----------------------------------
+
+using System.ComponentModel.DataAnnotations;
+using NosCore.Packets.Attributes;
+using NosCore.Packets.Enumerations;
+using NosCore.Packets.Interfaces;
+
+namespace NosCore.Packets.ServerPackets.UI
+{
+    [PacketHeader("dlgi2", Scope.InGame)]
+    public class Dlgi2Packet : PacketBase
+    {
+        [PacketIndex(0)]
+        public IPacket? YesPacket { get; set; }
+
+        [PacketIndex(1)]
+        public IPacket? NoPacket { get; set; }
+
+        [PacketIndex(2)]
+        public Game18NConstString Question { get; set; }
+
+        [PacketIndex(3)]
+        public byte ArgumentType { get; set; }
+
+        [Required]
+        [PacketIndex(4)]
+        public object[] Game18NArguments { get; set; } = null!;
+    }
+}
